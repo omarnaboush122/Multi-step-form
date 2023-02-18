15d4fc885cb5a78bf8957ff90c2729da0c34905e
omarnaboush122 <omarnaboush89@gmail.com>
2023-02-18 13:50:47 +0200

    import font and colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      Marineblue: "hsl(213, 96%, 18%)",
+      Purplishblue: "hsl(243, 100%, 62%)",
+      Pastelblue: "hsl(228, 100%, 84%)",
+      Lightblue: "hsl(206, 94%, 87%)",
+      Strawberryred: "hsl(354, 84%, 57%)",
+      Coolgray: "hsl(231, 11%, 63%)",
+      Lightgray: "hsl(229, 24%, 87%)",
+      Magnolia: "hsl(217, 100%, 97%)",
+      Alabaster: "hsl(231, 100%, 99%)",
+      White: "hsl(0, 0%, 100%)",
+    },
     extend: {},
   },
   plugins: [],
-}
+};

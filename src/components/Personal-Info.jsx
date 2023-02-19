15e83f803fc8b950e19a5af684e7c931458c9451
omarnaboush122 <omarnaboush89@gmail.com>
2023-02-19 08:51:55 +0200

    add style to personal info content

--- src/components/Personal-Info.jsx
+++ src/components/Personal-Info.jsx
@@ -1,20 +1,31 @@
 const PersonalInfo = () => {
   return (
     <div className="bg-Lightblue w-full h-96">
-      <div>
-        <h1>Personal info</h1>
-        <p>Please provide your name, email address, and phone number.</p>
-        <form>
-          <label htmlFor="name">Name</label>
-          <input type="text" placeholder="e.g. omar naboush" id="name" />
-          <label htmlFor="email">Email Address</label>
-          <input
-            type="email"
-            placeholder="e.g. omarnaboush509@gmail"
-            id="email"
-          />
-          <label htmlFor="phone">Phone Number</label>
-          <input type="number" placeholder="e.g. +961 81 346307" id="phone" />
+      <div className=" py-10 bg-[#f3f3f3] px-4 w-[90vw] mx-auto flex flex-col absolute top-24 left-5 rounded-xl">
+        <h1 className="mb-1 text-Marineblue text-3xl font-bold">
+          Personal info
+        </h1>
+        <p className="text-Coolgray leading-5">
+          Please provide your name, email address, and phone number.
+        </p>
+        <form className="flex flex-col gap-4 mt-5">
+          <div>
+            <label htmlFor="name" className="block mb-1 text-sm">Name</label>
+            <input className="w-full h-10 border border-Coolgray text-Marineblue px-3 rounded-md outline-none" type="text" placeholder="e.g. omar naboush" id="name" />
+          </div>
+          <div>
+            <label htmlFor="email" className="block mb-1 text-sm">Email Address</label>
+            <input
+            className="w-full h-10 border border-Coolgray text-Marineblue px-3 rounded-md outline-none"
+              type="email"
+              placeholder="e.g. omarnaboush509@gmail"
+              id="email"
+            />
+          </div>
+          <div>
+            <label htmlFor="phone" className="block mb-1 text-sm">Phone Number</label>
+            <input className="w-full h-10 border border-Coolgray text-Marineblue px-3 rounded-md outline-none" type="number" placeholder="e.g. +961 81 346307" id="phone" />
+          </div>
         </form>
       </div>
     </div>
--- src/components/Step.jsx
+++ src/components/Step.jsx
@@ -2,14 +2,13 @@ import { useContext } from "react";
 import { Context } from "../Context";
 
 const Step = ({ singleStep, i }) => {
-  const { step, setStep } = useContext(Context);
+  const { step } = useContext(Context);
 
   return (
     <div
       className={`w-8 h-8 rounded-full flex justify-center items-center mt-3 mr-5 border border-White cursor-pointer ${
         step === i + 1 ? "bg-Lightblue" : "text-White"
       }`}
-      onClick={() => setStep(i + 1)}
     >
       {singleStep}
     </div>
